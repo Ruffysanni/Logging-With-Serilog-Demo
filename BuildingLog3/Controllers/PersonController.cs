ce95c9dfe8a89b89cc661bbf6fa4a48baa6ef5cc
Rufai Sanni <sruffy07@gmail.com>
2023-06-30 15:19:42 +0100

    initial commit

---
+++ BuildingLog3/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingLog3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController>logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            try
+            {
+                int a = 3;
+                int c = a / 0;
+                    return Ok(c);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(500);
+            }
+        }
+    }
+}
